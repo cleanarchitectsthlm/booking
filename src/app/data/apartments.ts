6001c6bec1ccf7e14ec05750a02e76a05250857b
HannaTylna <hanna.tylna@gmail.com>
2023-01-09 12:56:06 +0100

    add apartments array

---
+++ src/app/data/apartments.ts
@@ -0,0 +1,62 @@
+const apartments = [
+  {
+    title: "Gallery M1 groundfloor",
+    images: [
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-11.jpg",
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-21.jpg",
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-31.jpg",
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-41.jpg",
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-51.jpg",
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-52.jpg",
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-5OLD.jpg",
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-61.jpg",
+      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-71.jpg",
+    ],
+    description: "Ground floor apartment, fitting families with children as there is more space to run around. Entrance from garden."
+  },
+  {
+    title: "Gallery M2 floor 1",
+    images: [
+      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-21.jpg",
+      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-31.jpg",
+      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-41.jpg",
+      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-51.jpg",
+      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-61.jpg",
+      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-71.jpg",
+      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-81.jpg",
+      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-91.jpg",
+    ],
+    description: "First floor apartment, sea view across the whole apartment with two large balconies."
+  },
+  {
+    title: "Gallery M3 floor 2",
+    images: [
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-1.jpg",
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-2.jpg",
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-3.jpg",
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-4.jpg",
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-5.jpg",
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-6.jpg",
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-7.jpg",
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-8.jpg",
+      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-9.jpg",
+    ],
+    description: "Second floor apartment with beautiful wide sea view over Rukavac. 15+ sqm spacious balcony and high ceilings that give feeling of more space."
+  },
+  {
+    title: "Gallery M4 floor 1",
+    images: [
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-11.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-21.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-31.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-41.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-51.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-61.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-71.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-81.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-91.jpg",
+      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-99.jpg",
+    ],
+    description: "Second floor apartment with beautiful loft. 12 sqm large balcony partial sea view over Rukavac. High ceilings that give feeling of more space."
+  }
+]
