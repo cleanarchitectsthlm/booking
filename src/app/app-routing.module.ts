1d205e6ac55ac114e406570c3e3d07cb009ed30e
HannaTylna <hanna.tylna@gmail.com>
2023-01-15 21:45:28 +0100

    add apartment router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { ExcursionsComponent } from './excursions/excursions.component';
 import { TransferComponent } from './transfer/transfer.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { ApartmentComponent } from './apartment/apartment.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'apartment/:id',
+    component: ApartmentComponent
   },
   {
     path: 'excursions',
