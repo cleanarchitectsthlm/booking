b3c27ccb075cfb60de218fb2152ea459c46bde9a
HannaTylna <hanna.tylna@gmail.com>
2023-01-15 23:25:34 +0100

    change resolve

--- src/app/resolvers/apartment-resolver.resolver.ts
+++ src/app/resolvers/apartment-resolver.resolver.ts
@@ -2,23 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {
   Router, Resolve,
-  RouterStateSnapshot,
   ActivatedRouteSnapshot
 } from '@angular/router';
-import { Observable, of } from 'rxjs';
 import { APARTMENTS } from '../data/apartments';
 
 @Injectable(
-  // {
-  // providedIn: 'root'
-  // }
+  {
+  providedIn: 'root'
+  }
 )
 export class ApartmentResolverResolver implements Resolve<any> {
   constructor(private http: HttpClient) { }
 
   public mockApartments: Array<any> = APARTMENTS;
-  
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
+
+  public getApartment(id: any): any {
+    let apartment: any = this.mockApartments.find(apartment => apartment.apartmentId == id);
+    return apartment;
+  }
+  resolve(route: ActivatedRouteSnapshot){
+    let id: any = route.params['id'];
+    return this.getApartment(id);
   }
 }
