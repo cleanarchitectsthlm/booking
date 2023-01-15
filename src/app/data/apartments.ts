a9a0da3d628c82846b0dc305aa0fa50c7316346b
HannaTylna <hanna.tylna@gmail.com>
2023-01-15 21:45:40 +0100

    add id

--- src/app/data/apartments.ts
+++ src/app/data/apartments.ts
@@ -1,5 +1,8 @@
-export const APARTMENTS = [
+import { Apartment } from "../models/apartment-model";
+
+export const APARTMENTS: Apartment[] = [
   {
+    apartmentId: 1,
     title: "Gallery M1 groundfloor",
     header: "From 100 € /night",
     picture: "../../assets/img/ap1.jpg",
@@ -17,6 +20,7 @@ export const APARTMENTS = [
     description: "Ground floor apartment, fitting families with children as there is more space to run around. Entrance from garden."
   },
   {
+    apartmentId: 2,
     title: "Gallery M2 floor 1",
     header: "From 120 € /night",
     picture: "../../assets/img/ap2.jpg",
@@ -33,6 +37,7 @@ export const APARTMENTS = [
     description: "First floor apartment, sea view across the whole apartment with two large balconies."
   },
   {
+    apartmentId: 3,
     title: "Gallery M3 floor 2",
     header: "From 110 € /night",
     picture: "../../assets/img/ap3.jpg",
@@ -50,6 +55,7 @@ export const APARTMENTS = [
     description: "Second floor apartment with beautiful wide sea view over Rukavac. 15+ sqm spacious balcony and high ceilings that give feeling of more space."
   },
   {
+    apartmentId: 4,
     title: "Gallery M4 floor 1",
     header: "From 110 € /night",
     picture: "../../assets/img/ap4.jpg",
