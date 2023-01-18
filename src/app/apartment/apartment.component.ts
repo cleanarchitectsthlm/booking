6655a831509bb220859d1af4de078257c338f3d1
HannaTylna <hanna.tylna@gmail.com>
2023-01-18 19:16:03 +0100

    add carousel

--- src/app/apartment/apartment.component.ts
+++ src/app/apartment/apartment.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-apartment',
@@ -8,8 +9,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ApartmentComponent {
   public apartment: any;
-  constructor(private route: ActivatedRoute) { }
-
+  title = 'angular-app';
+  images = [
+    {title: 'First Slide', desc: 'First Slide Description', src: "https://picsum.photos/id/102/900/500"},
+    {title: 'Second Slide', desc: 'Second Slide Description', src: "https://picsum.photos/id/1020/900/500"},
+    {title: 'Third Slide', desc: 'Third Slide Description', src: "https://picsum.photos/id/1024/900/500"}
+  ];
+  constructor(private route: ActivatedRoute, config: NgbCarouselConfig) {
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
   ngOnInit(): void {
     this.apartment = this.route.snapshot.data['apartment'];
   }
