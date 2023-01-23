34039ea566520e4dafc00f824a60460d4badcbc4
HannaTylna <hanna.tylna@gmail.com>
2023-01-23 21:19:28 +0100

    comment file

--- src/app/data/apartments.ts
+++ src/app/data/apartments.ts
@@ -1,76 +1,76 @@
-import { Apartment } from "../models/apartment-model";
+// import { Apartment } from "../models/apartment-model";
 
-export const APARTMENTS: Apartment[] = [
-  {
-    apartmentId: 1,
-    title: "Gallery M1 groundfloor",
-    header: "From 100 € /night",
-    picture: "../../assets/img/ap1.jpg",
-    images: [
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-11.jpg",
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-21.jpg",
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-31.jpg",
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-41.jpg",
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-51.jpg",
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-52.jpg",
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-5OLD.jpg",
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-61.jpg",
-      "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-71.jpg",
-    ],
-    description: "Ground floor apartment, fitting families with children as there is more space to run around. Entrance from garden."
-  },
-  {
-    apartmentId: 2,
-    title: "Gallery M2 floor 1",
-    header: "From 120 € /night",
-    picture: "../../assets/img/ap2.jpg",
-    images: [
-      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-21.jpg",
-      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-31.jpg",
-      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-41.jpg",
-      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-51.jpg",
-      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-61.jpg",
-      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-71.jpg",
-      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-81.jpg",
-      "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-91.jpg",
-    ],
-    description: "First floor apartment, sea view across the whole apartment with two large balconies."
-  },
-  {
-    apartmentId: 3,
-    title: "Gallery M3 floor 2",
-    header: "From 110 € /night",
-    picture: "../../assets/img/ap3.jpg",
-    images: [
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-1.jpg",
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-2.jpg",
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-3.jpg",
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-4.jpg",
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-5.jpg",
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-6.jpg",
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-7.jpg",
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-8.jpg",
-      "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-9.jpg",
-    ],
-    description: "Second floor apartment with beautiful wide sea view over Rukavac. 15+ sqm spacious balcony and high ceilings that give feeling of more space."
-  },
-  {
-    apartmentId: 4,
-    title: "Gallery M4 floor 1",
-    header: "From 110 € /night",
-    picture: "../../assets/img/ap4.jpg",
-    images: [
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-11.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-21.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-31.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-41.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-51.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-61.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-71.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-81.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-91.jpg",
-      "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-99.jpg",
-    ],
-    description: "Second floor apartment with beautiful loft. 12 sqm large balcony partial sea view over Rukavac. High ceilings that give feeling of more space."
-  }
-]
+// export const APARTMENTS: Apartment[] = [
+//   {
+//     apartmentId: 1,
+//     title: "Gallery M1 groundfloor",
+//     header: "From 100 € /night",
+//     picture: "../../assets/img/ap1.jpg",
+//     images: [
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-11.jpg",
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-21.jpg",
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-31.jpg",
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-41.jpg",
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-51.jpg",
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-52.jpg",
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-5OLD.jpg",
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-61.jpg",
+//       "../../assets/img/apartment_large/m1/visitaylinvis-apartment-m1-71.jpg",
+//     ],
+//     description: "Ground floor apartment, fitting families with children as there is more space to run around. Entrance from garden."
+//   },
+//   {
+//     apartmentId: 2,
+//     title: "Gallery M2 floor 1",
+//     header: "From 120 € /night",
+//     picture: "../../assets/img/ap2.jpg",
+//     images: [
+//       "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-21.jpg",
+//       "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-31.jpg",
+//       "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-41.jpg",
+//       "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-51.jpg",
+//       "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-61.jpg",
+//       "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-71.jpg",
+//       "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-81.jpg",
+//       "../../assets/img/apartment_large/m2/visitaylinvis-apartment-m2-91.jpg",
+//     ],
+//     description: "First floor apartment, sea view across the whole apartment with two large balconies."
+//   },
+//   {
+//     apartmentId: 3,
+//     title: "Gallery M3 floor 2",
+//     header: "From 110 € /night",
+//     picture: "../../assets/img/ap3.jpg",
+//     images: [
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-1.jpg",
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-2.jpg",
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-3.jpg",
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-4.jpg",
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-5.jpg",
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-6.jpg",
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-7.jpg",
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-8.jpg",
+//       "../../assets/img/apartment_large/m3/visitaylinvis-apartment-m3-9.jpg",
+//     ],
+//     description: "Second floor apartment with beautiful wide sea view over Rukavac. 15+ sqm spacious balcony and high ceilings that give feeling of more space."
+//   },
+//   {
+//     apartmentId: 4,
+//     title: "Gallery M4 floor 1",
+//     header: "From 110 € /night",
+//     picture: "../../assets/img/ap4.jpg",
+//     images: [
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-11.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-21.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-31.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-41.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-51.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-61.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-71.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-81.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-91.jpg",
+//       "../../assets/img/apartment_large/m4/visitaylinvis-apartment-m4-99.jpg",
+//     ],
+//     description: "Second floor apartment with beautiful loft. 12 sqm large balcony partial sea view over Rukavac. High ceilings that give feeling of more space."
+//   }
+// ]
