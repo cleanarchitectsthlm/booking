34d633c010e4830cc941380c5d288cd4956a519f
HannaTylna <hanna.tylna@gmail.com>
2023-01-23 21:21:50 +0100

    add function getApartments

--- src/app/Services/apartment.service.ts
+++ src/app/Services/apartment.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApartmentModel } from '../models/apartment-model';
+import { HttpClient } from '@angular/common/http';
+import { ConfigurationService } from './configuration.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ApartmentService {
+interface IApartmentService {
+  getApartments(): Observable<ApartmentModel[]>;
+}
+@Injectable()
+export class ApartmentService implements IApartmentService {
 
-  constructor() { }
+  constructor(private httpService: HttpClient, private configurationService: ConfigurationService) { }
+  public getApartments(): Observable<ApartmentModel[]> {
+    return this.httpService
+      .get<ApartmentModel[]>(this.configurationService.baseApiFnUrl + 'apartments');
+  }
 }
