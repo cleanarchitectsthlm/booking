984c33c4ca058c5ccb67fdd4d33b4a9d44259982
HannaTylna <hanna.tylna@gmail.com>
2023-01-24 09:54:14 +0100

    add api fetching

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { APARTMENTS } from '../data/apartments';
+// import { APARTMENTS } from '../data/apartments';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ApartmentModel } from '../models/apartment-model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,9 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  apartments = APARTMENTS;
+  private sub: any;
+  public apartmentList: ApartmentModel[];
+  // apartments = APARTMENTS;
   basicValue = '';
   rangeValue: { from: Date; to: Date } = {
     from: new Date(),
@@ -16,11 +20,16 @@ export class HomeComponent {
   };
 
   constructor(private route: ActivatedRoute,
-    private router: Router){}
+    private router: Router, private http: HttpClient){}
 
-  // ngOnInit(): void {
-  //   this.apartments = this.route.snapshot.data['apartments'];
-  // }
+  ngOnInit(): void {
+    this.sub = this.route.data
+      .subscribe(value => {
+          this.apartmentList = value['apartmentList'];
+        }
+      );
+    // this.apartments = this.route.snapshot.data['apartments'];
+  }
 
   public goToApartment(apartmentId: number) {
     this.router.navigate([`/apartment/${apartmentId}`]);
