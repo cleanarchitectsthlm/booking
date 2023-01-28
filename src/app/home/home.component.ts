65d313a11077619dbda9ea88b3f0bb194d6bada5
HannaTylna <hanna.tylna@gmail.com>
2023-01-28 19:25:30 +0100

    add handling review's api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ApartmentModel } from '../models/apartment-model';
 import { HttpClient } from '@angular/common/http';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subject } from 'rxjs';
+import { ReviewModel } from '../models/review-model';
+import { UserService } from '../Services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -20,8 +23,13 @@ export class HomeComponent {
     to: (new Date() as any)['fp_incr'](10)
   };
 
+  private reviewResources$: Subject<ReviewModel[]> = new Subject();
+  reviews: Observable<ReviewModel[]> | null = null;
+  reviewList: ReviewModel[];
+  selectedReview: ReviewModel;
+
   constructor(private route: ActivatedRoute,
-    private router: Router, private http: HttpClient, config: NgbCarouselConfig) {
+    private router: Router, private http: HttpClient, config: NgbCarouselConfig, private userService: UserService) {
       // customize default values of carousels used by this component tree
       config.interval = 10000;
       config.wrap = false;
@@ -36,6 +44,15 @@ export class HomeComponent {
         }
       );
     // this.apartments = this.route.snapshot.data['apartments'];
+    this.userService.getTopSixReviews().subscribe(res => {
+      this.reviewResources$.next(res);
+    });
+
+    this.reviews = this.reviewResources$;
+    this.reviews.subscribe(revs => {
+      this.reviewList = revs;
+      console.log(this.reviewList)
+    });
   }
 
   public goToApartment(apartmentId: number) {
