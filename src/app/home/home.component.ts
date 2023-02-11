fd9356cd435aeb99dafdcf33934b5b195280359d
HannaTylna <hanna.tylna@gmail.com>
2023-02-11 22:26:31 +0100

    add onSearch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 // import { APARTMENTS } from '../data/apartments';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { ApartmentModel } from '../models/apartment-model';
 import { HttpClient } from '@angular/common/http';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
@@ -16,6 +16,8 @@ import { UserService } from '../Services/user.service';
 export class HomeComponent {
   private sub: any;
   public apartmentList: ApartmentModel[];
+  checkinDate: Date;
+  checkoutDate: Date;
   // apartments = APARTMENTS;
   basicValue = '';
   rangeValue: { from: Date; to: Date } = {
@@ -55,6 +57,39 @@ export class HomeComponent {
     });
   }
 
+  isValidDate(d: any) {
+    const isnan = isNaN(d);
+    return d instanceof Date && !isnan;
+  }
+
+  onSearch() {
+    if (!this.isValidDate(this.checkinDate) || !this.isValidDate(this.checkoutDate)) {
+      return;
+    }
+
+    // Set our navigation extras object
+    // that contains our global query params and fragment
+    const in_yy = this.checkinDate.getFullYear();
+    const out_yy = this.checkoutDate.getFullYear();
+    const in_mm = this.checkinDate.getMonth() + 1;
+    const out_mm = this.checkoutDate.getMonth() + 1;
+    const in_dd = this.checkinDate.getDate();
+    const out_dd = this.checkoutDate.getDate();
+    let in_mm_str = in_mm.toString();
+    if (in_mm < 10) {
+      in_mm_str = '0' + in_mm.toString();
+    }
+    let out_mm_str = out_mm.toString();
+    if (out_mm < 10) {
+      out_mm_str = '0' + out_mm.toString();
+    }
+    const navigationExtras: NavigationExtras = {
+      queryParams: { 'checkin': in_yy + '-' + in_mm_str + '-' + in_dd, 'checkout': out_yy + '-' + out_mm_str + '-' + out_dd },
+      fragment: 'anchor'
+    };
+
+    this.router.navigate(['search'], navigationExtras);
+  }
   goToApartment(item: any) {
     this.router.navigate(['apartment', item.apartmentId]);
 }
