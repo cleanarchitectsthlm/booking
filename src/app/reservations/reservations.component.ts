54f1b4f693acb68a366dfcbcc1a7a0909ca16363
HannaTylna <hanna.tylna@gmail.com>
2023-02-21 10:50:07 +0100

    fetch information

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Reservation } from '../models/reservation-model';
+import { ReservationsService } from '../Services/reservations.service';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-reservations',
   templateUrl: './reservations.component.html',
   styleUrls: ['./reservations.component.css']
 })
-export class ReservationsComponent {
+export class ReservationsComponent implements OnInit {
+  private sub: any;
+  private reservationResources: Subject<Reservation[]> = new Subject();
+  reservations: Observable<Reservation[]> | null = null;
+  reservationList: Reservation[];
 
+  constructor( private route: ActivatedRoute, private reservationsService: ReservationsService) { }
+  ngOnInit(): void {
+    this.reservationsService.GetBookingReservations().subscribe(res => {
+      this.reservationResources.next(res);
+    });
+
+    this.reservations = this.reservationResources;
+    this.reservations.subscribe(reservation => {
+      this.reservationList = reservation;
+      console.log(this.reservationList)
+    });
+  }
 }
