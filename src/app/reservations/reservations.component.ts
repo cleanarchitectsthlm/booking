dfd324d6e356c5c35ae42f32f93f87fdf71eeb09
HannaTylna <hanna.tylna@gmail.com>
2023-03-02 21:58:39 +0100

    try to do an array from several arrays with dates

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Reservation } from '../models/reservation-model';
 import { ReservationsService } from '../Services/reservations.service';
 import { Observable, Subject } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-reservations',
@@ -14,6 +15,7 @@ export class ReservationsComponent implements OnInit {
   private reservationResources: Subject<Reservation[]> = new Subject();
   reservations: Observable<Reservation[]> | null = null;
   reservationList: Reservation[];
+  public days: string[];
 
   constructor( private route: ActivatedRoute, private reservationsService: ReservationsService) { }
   ngOnInit(): void {
@@ -26,5 +28,27 @@ export class ReservationsComponent implements OnInit {
       this.reservationList = reservation;
       console.log(this.reservationList)
     });
+
+    const getDaysByMonth = (month: any) => {
+      const daysInJune = moment(month).daysInMonth();
+      const arrayOfDays = Array.from({ length: daysInJune }, (v, k) => k + 1)
+      return arrayOfDays
+    };
+
+    const daysInJune = `${getDaysByMonth('2023-06')}`;
+    const daysInJuly = `${getDaysByMonth('2023-07')}`;
+    this.days = [daysInJune, daysInJuly];
+    console.log(this.days)
+    // let month = '2023-06';
+    // console.log(`June => ${getDaysByMonth('2023-06')}`);
+
+    // month = '2023-07';
+    // console.log(`July => ${getDaysByMonth(month)}`);
+
+    // month = '2023-08';
+    // console.log(`August => ${getDaysByMonth(month)}`);
+
+    // month = '2023-09';
+    // console.log(`September => ${getDaysByMonth(month)}`);
   }
 }
